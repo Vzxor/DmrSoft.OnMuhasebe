6785d61d508fdc80396175cd051ff5c8e13052d7
Vzxor <harundemir@outlook.com>
2024-04-26 15:46:53 +0300

    Initial commit

---
+++ src/DmrSoft.OnMuhasebe.EntityFrameworkCore/Kasalar/EfCoreKasaRepository.cs
@@ -0,0 +1,12 @@
+using DmrSoft.OnMuhasebe.Commons;
+using DmrSoft.OnMuhasebe.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace DmrSoft.OnMuhasebe.Kasalar;
+
+public class EfCoreKasaRepository:EfCoreCommonRepository<Kasa>
+{
+    public EfCoreKasaRepository(IDbContextProvider<OnMuhasebeDbContext> dbContextProvider) : base(dbContextProvider)
+    {
+    }
+}
