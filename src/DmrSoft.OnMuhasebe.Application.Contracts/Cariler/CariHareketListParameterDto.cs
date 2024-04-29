debba2f0a142e6cd0596173e1feb32c227ec142e
Vzxor <harundemir@outlook.com>
2024-04-29 17:17:11 +0300

    CariDto&CariHareketDto Islemleri

---
+++ src/DmrSoft.OnMuhasebe.Application.Contracts/Cariler/CariHareketListParameterDto.cs
@@ -0,0 +1,13 @@
+using System;
+using DmrSoft.OnMuhasebe.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace DmrSoft.OnMuhasebe.Cariler;
+
+public class CariHareketListParameterDto:PagedResultRequestDto,IDurum,IEntityDto
+{
+    public Guid CariId { get; set; }
+    public Guid SubeId { get; set; }
+    public Guid DonemId { get; set; }
+    public bool Durum { get; set; }
+}
