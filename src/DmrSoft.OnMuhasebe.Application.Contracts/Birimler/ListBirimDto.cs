8f2f4e9e25e858526283463067da4ef84d26b489
Vzxor <harundemir@outlook.com>
2024-04-29 11:43:26 +0300

    BirimlerDto Islemleri

---
+++ src/DmrSoft.OnMuhasebe.Application.Contracts/Birimler/ListBirimDto.cs
@@ -0,0 +1,14 @@
+using System;
+using Volo.Abp.Application.Dtos;
+
+namespace DmrSoft.OnMuhasebe.Birimler;
+
+public class ListBirimDto:EntityDto<Guid>
+{
+    public string Kod { get; set; }
+    public string Ad { get; set; }
+    public string OzelKod1Adi { get; set; }
+    public string OzelKod2Adi { get; set; }
+    public string Aciklama { get; set; }
+    public bool Durum { get; set; }
+}
