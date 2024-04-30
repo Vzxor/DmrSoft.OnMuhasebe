2bec500ffb693dc0f3e0a5f11b6eb051957a378b
Vzxor <harundemir@outlook.com>
2024-04-30 11:51:06 +0300

    FaturaDto&FaturaHareketDto Islemleri

---
+++ src/DmrSoft.OnMuhasebe.Application.Contracts/Faturalar/UpdateFaturaDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Volo.Abp.Application.Dtos;
+
+namespace DmrSoft.OnMuhasebe.Faturalar;
+
+public class UpdateFaturaDto:IEntityDto
+{
+    public Guid CariId { get; set; }
+    public string FaturaNo { get; set; }
+    public DateTime Tarih { get; set; }
+    public decimal BrutTutar { get; set; }
+    public decimal IndirimTutar { get; set; }
+    public decimal KdvHaricTutar { get; set; }
+    public decimal KdvTutar { get; set; }
+    public decimal NetTutar { get; set; }
+    public int HareketSayisi { get; set; }
+    public Guid? OzelKod1Id { get; set; }
+    public Guid? OzelKod2Id { get; set; }
+    public string Aciklama { get; set; }
+    public bool Durum { get; set; }
+    public List<FaturaHareketDto> FaturaHareketler { get; set; }
+}
