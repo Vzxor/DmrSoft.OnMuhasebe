90fd98d438afc37b9b255746107f2c2e204f6dbd
Vzxor <harundemir@outlook.com>
2024-04-30 14:19:31 +0300

    BankaDto Islemleri

--- src/DmrSoft.OnMuhasebe.Application.Contracts/Bankalar/SelectBankaDto.cs
+++ src/DmrSoft.OnMuhasebe.Application.Contracts/Bankalar/SelectBankaDto.cs
@@ -12,7 +12,6 @@ public class SelectBankaDto : EntityDto<Guid>, IOzelKod
     public string OzelKod1Adi { get; set; }
     public Guid? OzelKod2Id { get; set; }
     public string OzelKod2Adi { get; set; }
-    public Guid SubeId { get; set; }
     public string Aciklama { get; set; }
     public bool Durum { get; set; }
 }
